c8ff3845c88d93ba89ea70c5e7de1813ae75c4f6
100nuparmar349 <sonuparmar7224@gmail.com>
2025-02-09 15:29:12 +0530

    make the page responsive

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "/",  // Make sure assets load from the correct path
+  build: {
+    outDir: "dist",
+  },
 })
