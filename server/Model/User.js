15acfb3246b3e15ab29d24de6e0844d9ba3ed667
sonuparmar <140191471+100nuparmar349@users.noreply.github.com>
2025-02-10 14:41:21 +0530

    Delete server directory

--- server/Model/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-let userSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    Email:{type:String,required:true,unique:true},
-    message:{type:String}
-});
-const User=mongoose.model("User",userSchema);
-module.exports=User;
